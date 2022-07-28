f0dd1075a11fd83c85bbaac6d3a6aececc536c79
Viktor Denikin <leshden@yandex.ru>
2022-07-28 20:23:40 +0300

    fix test

--- src/features/game-state/gameState.test.js
+++ src/features/game-state/gameState.test.js
@@ -1,21 +1,27 @@
 import gameStateReducer, {
   startGame,
+  GAME_STATUS_PROCESSING,
+  GAME_STATUS_IDLE,
 } from './gameStateSlice';
 
 describe('game state reducer', () => {
   const initialState = {
-    start: false,
+    startIndex: -1,
+    loseIndex: -1,
+    index: -1,
+    steps: [],
+    status: GAME_STATUS_IDLE,
+    size: 3,
+    maxSteps: 5,
   };
 
   it('initial state', () => {
-    expect(gameStateReducer(undefined, { type: 'unknown' })).toEqual({
-      start: false,
-    });
+    expect(gameStateReducer(undefined, { type: 'unknown' })).toEqual(initialState);
   });
 
   it('handle startGame', () => {
     const actual = gameStateReducer(initialState, startGame());
-    expect(actual.start).toEqual(true);
+    expect(actual.status).toEqual(GAME_STATUS_PROCESSING);
   });
 
   });
